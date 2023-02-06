8daf84b11489af53425f44b5d702f5d6d3f7290c
kankit08 <kankit08@gmail.com>
2023-02-06 18:52:38 +0530

    movie file done

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,18 +1,27 @@
 import React, { useContext } from "react";
 import { AppContext } from "../Context";
-
+import { NavLink } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
 const Movie = () => {
   const { movie } = useContext(AppContext);
   return (
-    <div>
-      {movie.map((movieList) => {
-        return (
-          <div>
-            <h2>{movieList.Title}</h2>
-          </div>
-        );
-      })}
-    </div>
+    <section className="movie-page">
+      <div className="container grid grid-4-col">
+        {movie.map((movieList) => {
+          const { imdbID, Title, Poster } = movieList;
+          return (
+            <NavLink to={`movie/${imdbID}`} key={uuidv4()}>
+              <div className="card">
+                <div className="card-info">
+                  <h2>{Title}</h2>
+                  <img src={Poster} alt={imdbID} />
+                </div>
+              </div>
+            </NavLink>
+          );
+        })}
+      </div>
+    </section>
   );
 };
 
